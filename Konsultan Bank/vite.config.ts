10bc98e8125378b112d3477217ab07fd87bada0a
ATP Showcase <atpshowcase@gmail.com>
2026-03-13 20:45:14 +0700

    p

--- Konsultan Bank/vite.config.ts
+++ Konsultan Bank/vite.config.ts
@@ -7,6 +7,17 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 export default defineConfig({
   base: './',
   plugins: [inspectAttr(), react()],
+  server: {
+    port: 2007,
+    host: true,
+    open: true,
+    strictPort: true,
+    allowedHosts: [
+      'atpshowcase.com',
+      'www.atpshowcase.com',
+      'bank.atpshowcase.com'
+    ]
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
